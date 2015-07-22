575a3cfeda034ccb0d5c52e27c3032c1c263c435
Nick Leach <nickleach22@gmail.com>
2015-07-22 09:51:38 -0400

    Added parallax imgs, staggered list

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@
   'use strict';
   //Initialize Materialze Functions
 
+
     $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
+
+    setTimeout(function() {
+      Materialize.showStaggeredList('#staggered-list');
+    }, 2000);
+
+// class to add to nav-header : light-blue darken-4 and nav-wrapper
 
   // Add active class on click nav bar
 
