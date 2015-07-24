da94a8354da235173f32b1aea7286b0b8f0ff2d8
Nick Leach <nickleach22@gmail.com>
2015-07-24 12:02:34 -0400

    First draft

--- js/main.js
+++ js/main.js
@@ -11,9 +11,20 @@
       Materialize.showStaggeredList('#staggered-list');
     }, 2000);
 
+    var navChange = '$("nav").removeClass("transparent").addClass("nav-header-scroll")';
+
+
+    var options = [
+    {selector: '.image-fades', offset: 200, callback: 'Materialize.fadeInImage(".image-fades")' },
+    {selector: '.nav-header', offset: 500, callback: navChange}
+  ];
+    Materialize.scrollFire(options);
+
+    $('.modal-trigger').leanModal();
 // class to add to nav-header : light-blue darken-4 and nav-wrapper
 
   // Add active class on click nav bar
+
 
   $('#nav-mobile').on('click', 'a', function(){
     $('li').removeClass('active');
